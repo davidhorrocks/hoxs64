77287ba4267c0119fae1ac6083311b8b9d0a633c
David Horrocks <davidhorrocks@btinternet.com>
2011-12-26 15:13:28 +0000

    Separated CDPI definition and declaration.

--- hoxs64/CDPI.cpp
+++ hoxs64/CDPI.cpp
@@ -1,3 +1,6 @@
+#include <windows.h>
+#include <windowsx.h>
+#include "CDPI.h"
 //+---------------------------------------------------------------------------
 //	
 //  Class:
@@ -33,67 +36,92 @@
 //	PARTICULAR PURPOSE.
 //----------------------------------------------------------------------------
 
-class CDPI
-{
-public:
-    CDPI();
+CDPI::CDPI() : m_fInitialized(false), m_dpiX(96), m_dpiY(96) { }
     
     // Get screen DPI.
-    int GetDPIX();
-    int GetDPIY();
+    int CDPI::GetDPIX() { _Init(); return m_dpiX; }
+    int CDPI::GetDPIY() { _Init(); return m_dpiY; }
 
     // Convert between raw pixels and relative pixels.
-    int ScaleX(int x);
-    int ScaleY(int y);
-    int UnscaleX(int x);
-    int UnscaleY(int y);
+    int CDPI::ScaleX(int x) { _Init(); return MulDiv(x, m_dpiX, 96); }
+    int CDPI::ScaleY(int y) { _Init(); return MulDiv(y, m_dpiY, 96); }
+    int CDPI::UnscaleX(int x) { _Init(); return MulDiv(x, 96, m_dpiX); }
+    int CDPI::UnscaleY(int y) { _Init(); return MulDiv(y, 96, m_dpiY); }
 
     // Determine the screen dimensions in relative pixels.
-    int ScaledScreenWidth();
-    int ScaledScreenHeight();
+    int CDPI::ScaledScreenWidth() { return _ScaledSystemMetricX(SM_CXSCREEN); }
+    int CDPI::ScaledScreenHeight() { return _ScaledSystemMetricY(SM_CYSCREEN); }
 
     // Scale rectangle from raw pixels to relative pixels.
-    void ScaleRect(__inout RECT *pRect);
+    void CDPI::ScaleRect(__inout RECT *pRect)
+    {
+        pRect->left = ScaleX(pRect->left);
+        pRect->right = ScaleX(pRect->right);
+        pRect->top = ScaleY(pRect->top);
+        pRect->bottom = ScaleY(pRect->bottom);
+    }
 
     // Scale Point from raw pixels to relative pixels.
-    void ScalePoint(__inout POINT *pPoint);
+    void CDPI::ScalePoint(__inout POINT *pPoint)
+    {
+        pPoint->x = ScaleX(pPoint->x);
+        pPoint->y = ScaleX(pPoint->y);        
+    }
 
     // Scale Size from raw pixels to relative pixels.
-    void ScaleSize(__inout SIZE *pSize);
+    void CDPI::ScaleSize(__inout SIZE *pSize)
+    {
+        pSize->cx = ScaleX(pSize->cx);
+        pSize->cy = ScaleX(pSize->cy);		
+    }
 
     // Determine if screen resolution meets minimum requirements in relative pixels.
-    bool IsResolutionAtLeast(int cxMin, int cyMin);
+    bool CDPI::IsResolutionAtLeast(int cxMin, int cyMin) 
+    { 
+        return (ScaledScreenWidth() >= cxMin) && (ScaledScreenHeight() >= cyMin); 
+    }
 
     // Convert a point size (1/72 of an inch) to raw pixels.
-    int PointsToPixels(int pt);
+    int CDPI::PointsToPixels(int pt) { return MulDiv(pt, m_dpiY, 72); }
 
     // Invalidate any cached metrics.
-    void Invalidate();
-
-private:
+    void CDPI::Invalidate() { m_fInitialized = false; }
 
     // This function initializes the CDPI Class
-    void _Init();
+    void CDPI::_Init()
+    {
+        if (!m_fInitialized)
+        {
+            HDC hdc = GetDC(NULL);
+            if (hdc)
+            {
+                // Initialize the DPI member variable
+                // This will correspond to the DPI setting
+                // With all Windows OS's to date the X and Y DPI will be identical					
+                m_dpiX = GetDeviceCaps(hdc, LOGPIXELSX);
+                m_dpiY = GetDeviceCaps(hdc, LOGPIXELSY);
+                ReleaseDC(NULL, hdc);
+            }
+            m_fInitialized = true;
+        }
+    }
 
     // This returns a 96-DPI scaled-down equivalent value for nIndex 
     // For example, the value 120 at 120 DPI setting gets scaled down to 96		
     // X and Y versions are provided, though to date all Windows OS releases 
     // have equal X and Y scale values
-    int _ScaledSystemMetricX(int nIndex);
+    int CDPI::_ScaledSystemMetricX(int nIndex) 
+    { 
+        _Init(); 
+        return MulDiv(GetSystemMetrics(nIndex), 96, m_dpiX); 
+    }
 
     // This returns a 96-DPI scaled-down equivalent value for nIndex 
     // For example, the value 120 at 120 DPI setting gets scaled down to 96		
     // X and Y versions are provided, though to date all Windows OS releases 
     // have equal X and Y scale values
-    int _ScaledSystemMetricY(int nIndex);
-
-private:
-        
-    // Member variable indicating whether the class has been initialized
-    bool m_fInitialized; 
-
-    // X and Y DPI values are provided, though to date all 
-    // Windows OS releases have equal X and Y scale values
-    int m_dpiX;			
-    int m_dpiY;
-};
+    int CDPI::_ScaledSystemMetricY(int nIndex) 
+    { 
+        _Init(); 
+        return MulDiv(GetSystemMetrics(nIndex), 96, m_dpiY); 
+    }
