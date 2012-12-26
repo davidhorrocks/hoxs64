49bf3cca7522d8fe903d46d4bac2e9eb7ddefbf8
David Horrocks <davidhorrocks@btinternet.com>
2012-12-26 17:26:15 +0000

    Fixed VIC bank bug that was introduced with the new Ultimax cartridge VIC character ROM fetch logic.

--- hoxs64/ram64.cpp
+++ hoxs64/ram64.cpp
@@ -973,7 +973,7 @@ void RAM64::ConfigureVICMMU(bit8 index, bit8 ***p_vic_memory_map_read, bit8 **p_
 	else
 	{
 		VicMMU_read[0x0][0x3] = mMemory;
-		VicMMU_read[0x2][0x3] = mMemory;
+		VicMMU_read[0x2][0x3] = mMemory+0x8000;
 	}
 	*p_vic_memory_map_read = VicMMU_read[index & 3];
 	*p_vic_3fff_ptr = &(*p_vic_memory_map_read)[3][0x3fff];
