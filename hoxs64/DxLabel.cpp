ac3f0af8e3f3d7f043288dd0c8a9c900b7d44891
David Horrocks <davidhorrocks@btinternet.com>
2015-01-08 22:06:57 +0000

    Tidy DxWindow

---
+++ hoxs64/DxLabel.cpp
@@ -0,0 +1,36 @@
+#include <windows.h>
+#include <windowsx.h>
+#include <commctrl.h>
+#include "dx_version.h"
+#include <d3d9.h>
+#include <d3dx9core.h>
+#include "boost2005.h"
+#include <tchar.h>
+#include <string.h>
+#include "DxWindow.h"
+
+DxLabel::DxLabel()
+{
+}
+
+DxLabel::~DxLabel()
+{
+}
+
+void DxLabel::Draw()
+{
+HRESULT hr;
+	if (m_sprMessageText != NULL)
+	{
+		if (SUCCEEDED(hr = m_sprMessageText->Begin(D3DXSPRITE_ALPHABLEND | D3DXSPRITE_SORT_TEXTURE)))
+		{
+			RECT rc;
+			if (SUCCEEDED(GetTextRect(&rc)))
+			{
+				SetRect(&rc, xpos, ypos, 0, 0);
+				m_dxfont->DrawText(m_sprMessageText, m_psztext, -1, &rc, DT_NOCLIP, D3DXCOLOR( 1.0f, 1.0f, 1.0f, 1.0f ));
+			}
+			m_sprMessageText->End();
+		}
+	}
+}
---
+++ hoxs64/DxTexture.cpp
@@ -0,0 +1,44 @@
+#include <windows.h>
+#include <windowsx.h>
+#include <commctrl.h>
+#include "dx_version.h"
+#include <d3d9.h>
+#include <d3dx9core.h>
+#include "boost2005.h"
+#include <tchar.h>
+#include <string.h>
+#include "DxWindow.h"
+
+DxTexture::DxTexture()
+{
+	m_resourceid = 0;
+	m_ptx = NULL;
+}
+
+DxTexture::~DxTexture()
+{
+}
+
+void DxTexture::FreeDeviceObjects()
+{
+	if (m_ptx!=NULL)
+	{
+		m_ptx->Release();
+		m_ptx = NULL;
+	}
+}
+
+HRESULT DxTexture::LoadDeviceObjects()
+{
+	return S_OK;
+}
+
+void DxTexture::OnLostDevice()
+{
+}
+void DxTexture::OnResetDevice()
+{
+}
+void DxTexture::SetResourceId()
+{
+}
